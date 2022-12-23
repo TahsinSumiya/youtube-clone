d4a41630856a5156da0efc43e84fbce96ad2c8e4
TahsinSumiya <tahsinadity187@gmail.com>
2022-12-24 00:40:38 +0600

    material ui

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,10 +1,33 @@
-import React from 'react'
-
+import {useEffect,useState} from 'react'
+import { Box, Stack, Typography } from "@mui/material";
+import { MpSharp } from '@mui/icons-material'
+import Sidebar from './Sidebar';
+import Videos from './Videos';
 function Feed() {
   return (
-    <div>
-      F
-    </div>
+    <Stack sx={{flexDirection:{
+        sx:"column",
+        md:"row"
+    }}}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" },
+       borderRight: "1px solid #3d3d3d",
+        px: { sx: 0, md: 2 } }}>
+        <Sidebar/>
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright 2022| by Tahsin Sumiya
+        </Typography>
+      </Box>
+
+
+      <Box p={2} sx={{overflowY:'auto',height:'90vh',flex:2}}>
+        <Typography variant='h5' fontWeight="bold"mb={2}sx={{
+            color:'white'
+        }}>
+            New<span style={{color:'#F31503'}}>VIDEOS</span>
+        </Typography>
+        <Videos/>
+      </Box>
+    </Stack>
   )
 }
 
